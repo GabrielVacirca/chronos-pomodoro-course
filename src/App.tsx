31dd2d695811b53375a173fca968e4b911a7a634
Gabriell David <vacircagabriel21@gmail.com>
2026-07-16 00:28:08 -0300

    feat: Criando o formulário da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,31 @@ export function App() {
         <Menu />
       </Container>
       <Container>
-       
-          <CountDown />
-        
+        <CountDown />
+      </Container>
+      <Container>
+        <form className="form">
+          <div className="formRow">
+            <label htmlFor="meuInput">task</label>
+            <input id="meuInput" className="input" type="text"></input>
+          </div>
+
+          <div className="formRow">
+            <label htmlFor="meuInput">Lorem ipsum dolor sit amet.</label>
+            
+          </div>
+
+           <div className="formRow">
+            <p>Lorem ipsum dolor sit.</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+           <div className="formRow">
+            <button>Enviar</button>
+          </div>
+
+
+        </form>
       </Container>
     </>
   );
