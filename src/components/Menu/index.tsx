5706a84c33e239e5f88357a464929f741b2ebf1c
Gabriell David <vacircagabriel21@gmail.com>
2026-07-24 12:22:17 -0300

    feat: usando useState e useEffect para trocar o tema e salvar no localStorage

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,44 @@
-import { House, Settings, History, Sun } from "lucide-react";
+import { House, Settings, History, SunIcon, MoonIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+type AvailableThemes = "light" | "dark";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = localStorage.getItem("theme") as AvailableThemes || 'dark';
+     
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, // Adiciona o tipo correto para o evento
+  ) {
+    event.preventDefault(); // Impede que o link seja seguido
+    console.log("Theme changed");
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    console.log("O tema mudou para:", theme);
+    localStorage.setItem("theme", theme); // Salva o tema atual no localStorage
+    document.documentElement.setAttribute("data-theme", theme); // Atualiza o atributo data-theme do elemento <html> com o valor do tema atual
+
+    return () => {
+      // Função de limpeza que será executada antes do próximo efeito ou quando o componente for desmontado
+      console.log("Cleanup: o tema anterior era:", theme);
+    };
+  }, [theme]); // executa apenas quando o valor do tema muda
+
   return (
     <nav className={styles.menu}>
       <a href="#" className={styles.menuLogo}>
@@ -13,8 +50,8 @@ export function Menu() {
       <a href="#" className={styles.menuLogo}>
         <Settings />
       </a>
-      <a href="#" className={styles.menuLogo}>
-        <Sun />
+      <a href="#" className={styles.menuLogo} onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
